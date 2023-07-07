d9e310aa7b042dfd300f742dd108ad935fced8b9
ovaidov <106985580+YoadNatan@users.noreply.github.com>
2023-07-07 12:05:12 +0300

    Revert "Merge branch 'main' into OC"
    This reverts commit 8ecc8f03f2984883b2dfb37b75d583b7027af8b4, reversing
    changes made to 4e628d928c0071483d29b07f76a533f62d65d53a.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,36 +1,73 @@
-import React, { useState, useEffect, useContext } from "react";
-import { Grid, Typography, Box } from "@mui/material";
+import { useState, useEffect, useContext } from "react";
+import {
+  Grid,
+  Typography,
+  Box,
+  Button,
+  TextField,
+  Card,
+  CardContent,
+  List,
+  ListItem,
+} from "@mui/material";
 import { MainserverContext } from "@failean/mainserver-provider";
-import axios from "axios";
-import TokenAnalytics from "../TokenAnalytics";
-import { TokenData } from "../TokenAnalytics";
+import InvalidPromptEvents from "../InvalidPromptEvents";
 
-function App() {
-  const [status, setStatus] = useState("pending");
-<<<<<<< HEAD
-  const [tokenData, setTokenData] = useState<TokenData[]>([]); // New state for tokenData
-  const mainServer = useContext(MainserverContext);
+type Login = {
+  name: string;
+};
 
+const App = () => {
+  // Add states for the new functionalities
+  const [lastDayLogins, setLastDayLogins] = useState<Login[]>([]);
+  const [invalidPromptEvents, setInvalidPromptEvents] = useState([]);
+  const [avgPrice, setAvgPrice] = useState(0);
+  const [promptName, setPromptName] = useState("examplePrompt");
+
+  const mainServer = useContext(MainserverContext); // Define mainServer here
+  const [status, setStatus] = useState("pending"); // Define status and setStatus here
+
+  // Add effects to fetch the data from the backend
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const { data } = await axios.get(
-          "http://localhost:6777/api/token-analytics" // Update the URL with your oc-server endpoint
-        );
-        setTokenData(data);
-      } catch (error) {
-        console.error("Error fetching token analytics data:", error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  useEffect(() => {
-    const checkServerStatus = async () => {
+    const fetchLastDayLogins = async () => {
       if (mainServer) {
         const { axiosInstance } = mainServer;
         try {
+          const { data } = await axiosInstance.get("/LastDayLogins");
+          setLastDayLogins(data);
+        } catch (error) {
+          console.error("Error fetching last day logins data:", error);
+        }
+      }
+    };
+
+    fetchLastDayLogins();
+  }, [mainServer]);
+
+  useEffect(() => {
+    const fetchInvalidPromptEvents = async () => {
+      if (mainServer) {
+        const { axiosInstance } = mainServer;
+        try {
+          const { data } = await axiosInstance.get("/InvalidPromptEvents");
+          setInvalidPromptEvents(data.events);
+        } catch (error) {
+          console.error("Error fetching invalid prompt events data:", error);
+        }
+      }
+    };
+
+    fetchInvalidPromptEvents();
+  }, [mainServer]);
+
+  useEffect(() => {
+    const checkServerStatus = async () => {
+      debugger;
+
+      if (mainServer) {
+        const { axiosInstance } = mainServer;
+        try {
+          debugger;
           const { data } = await axiosInstance.get("/areyoualive");
           setStatus(data.answer === "yes" ? "working" : "not working");
         } catch (e) {
@@ -40,53 +77,101 @@ function App() {
     };
     checkServerStatus();
   }, [mainServer]);
-=======
 
-  const mainserverContext = useContext(MainserverContext);
-  const axiosInstance = mainserverContext?.axiosInstance;
-
-  useEffect(() => {
-    const check = async () => {
-      if (axiosInstance)
-        try {
-          const { data } = await axiosInstance.get("/areyoualive");
-          setStatus(data.answer === "yes" ? "working" : "not working");
-        } catch (e) {
-          setStatus("not working");
-        }
-    };
-
-    check();
-  }, [axiosInstance]);
->>>>>>> 7f87f5052a7d3cbfbf90713f629398e4c9cdb158
+  const fetchAvgPriceForPrompt = async () => {
+    if (mainServer) {
+      const { axiosInstance } = mainServer;
+      try {
+        const { data } = await axiosInstance.post("/AvgPriceForPrompt", {
+          promptName,
+        });
+        setAvgPrice(data.avg);
+      } catch (error) {
+        console.error("Error fetching average price data:", error);
+      }
+    }
+  };
 
   return (
-    <Grid container direction="column">
+    <Grid container direction="column" spacing={3}>
       <Grid item>
-        <Typography>Hello</Typography>
+        <Typography variant="h4">Welcome to OC Client Dashboard!</Typography>
       </Grid>
 
       <Grid item>
-        <Box
-          bgcolor={
-            status === "working"
-              ? "green"
-              : status === "pending"
-              ? "yellow"
-              : "red"
-          }
-          height="100px"
-          width="500px"
-        >
-          Server status is {status}
-        </Box>
+        <Card>
+          <CardContent>
+            <Typography variant="h6">Server Status Is:</Typography>
+            <Box
+              bgcolor={
+                status === "working"
+                  ? "green"
+                  : status === "pending"
+                  ? "yellow"
+                  : "red"
+              }
+              height="8rem"
+              width="30%"
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+              borderRadius="20px"
+              color="black"
+            >
+              {status.toUpperCase()}
+            </Box>
+          </CardContent>
+        </Card>
       </Grid>
 
       <Grid item>
-        <TokenAnalytics tokenData={tokenData} /> {/* Pass tokenData to TokenAnalytics */}
+        <InvalidPromptEvents events={invalidPromptEvents} />
+      </Grid>
+
+      <Grid item>
+        <Card>
+          <CardContent>
+            <Typography variant="h6">Users who Logged in Last Day</Typography>
+            <List>
+              {lastDayLogins.map((user, index) => (
+                <ListItem key={index}>{user.name}</ListItem>
+              ))}
+            </List>
+          </CardContent>
+        </Card>
+      </Grid>
+
+      <Grid item>
+        <Card>
+          <CardContent>
+            <Typography variant="h6">Fetch Average Price</Typography>
+            <TextField
+              label="Prompt Name"
+              variant="outlined"
+              value={promptName}
+              onChange={(e) => setPromptName(e.target.value)}
+            />
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => {
+                if (promptName) {
+                  fetchAvgPriceForPrompt();
+                } else {
+                  alert("Please enter a prompt name!");
+                }
+              }}
+            >
+              Get Average Price
+            </Button>
+            <Typography variant="subtitle1">
+              Average Price: {avgPrice}
+            </Typography>
+          </CardContent>
+        </Card>
       </Grid>
     </Grid>
   );
-}
+};
 
 export default App;
