71ed24662a3d847244e3066d7c887ecd695c2c9b
ovaidov <110204739+ovaidov@users.noreply.github.com>
2023-07-13 09:42:58 +0300

    s

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,15 @@
 import Signins from "./components/kpis/Signins";
 import Servers from "./components/status/Server";
 import InvalidPromptEvents from "./components/kpis/InvalidPromptEvents";
+import AvgPriceForPrompt from "./components/kpis/AvgPriceForPrompt";
 const App = () => {
   return (
     <>
       <Servers />
       <Signins />
     <InvalidPromptEvents  />
+    <AvgPriceForPrompt />
+
     </>
   );
 };
---
+++ src/app/components/kpis/AvgPriceForPrompt.tsx
@@ -0,0 +1,39 @@
+import { useState, useEffect, useContext } from "react";
+import { OcserverContext } from "../../context/OcserverContext";
+import { Typography, Card, CardContent } from "@mui/material";
+
+const AvgPriceForPrompt = () => {
+  const [avgPrice, setAvgPrice] = useState<number | null>(null);
+
+  const ocserverContext = useContext(OcserverContext);
+  const axiosInstance = ocserverContext?.axiosInstance;
+
+  useEffect(() => {
+    const fetchAveragePrice = async () => {
+      try {
+        if (axiosInstance) {
+          const promptNames = ["example_prompt"]; 
+          const res = await axiosInstance.post("/avgPriceForPrompt", { promptName: promptNames });
+          const { avg } = res.data;
+          setAvgPrice(avg || null);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchAveragePrice();
+  }, [axiosInstance]);
+
+  return (
+    <div>
+      <Typography variant="h6">Average Price for Prompt: {avgPrice || "N/A"}</Typography>
+      <Card>
+        <CardContent>
+          {/* Additional content for the card */}
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default AvgPriceForPrompt;
