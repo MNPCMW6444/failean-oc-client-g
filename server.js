1b9a7f49e79d51a33152a61fb34fca2c4fd9cb13
Michael Nusair‬‏ <mnpcmw6444@gmail.com>
2023-08-10 17:27:41 +0300

    hc

--- server.js
+++ server.js
@@ -11,8 +11,12 @@ const PASSWORD = "faileanizem8000";
 
 
 app.use((req, res, next) => {
-    const credentials = basicAuth(req);
+    // If the route is /health-check, bypass authentication
+    if (req.path === '/health-check') {
+        return next();
+    }
 
+    const credentials = basicAuth(req);
     if (credentials && credentials.name === USERNAME && credentials.pass === PASSWORD) {
         return next();
     }
@@ -21,6 +25,10 @@ app.use((req, res, next) => {
     res.status(401).send('Authentication required');
 });
 
+
+app.get('/health-check', (_, res) => {
+    res.status(200).send('Server is healthy');
+});
 
 
 
